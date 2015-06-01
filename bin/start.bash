04c37d6c59691ce0f52c7884b6662b9efa33fe5d
misterbisson <casey.bisson@gmail.com>
2015-06-01 15:52:33 +0000

    wired to Consul for discovery

--- bin/start.bash
+++ bin/start.bash
@@ -3,12 +3,39 @@
 echo "Uncompressing source data..."
 cd /data && ./expand.sh
 
-echo "Creating the bucket"
-BUCKET_RAM=100
-curl -i -X POST -u Administrator:password -d name=newbucket -d ramQuotaMB=$BUCKET_RAM -d authType=sasl -d saslPassword='' -d replicaNumber=1 http://$COUCHBASE_HOST:8091/pools/default/buckets
+echo '#'
+echo '# Looking for the Couchbase cluster'
+echo '#'
 
-echo "Setting up views..."
-cloud-benchmark setup -c couchbase://$COUCHBASE_HOST
+CLUSTERFOUND=0
+while [ $CLUSTERFOUND != 1 ]; do
+    echo -n '.'
 
-echo "Executing benchmarks..."
-cloud-benchmark run -d /data -c couchbase://$COUCHBASE_HOST
+    CLUSTERIP=$(curl -sL http://consul:8500/v1/catalog/service/couchbase | json -aH ServiceAddress | head -1)
+    if [ -n "$CLUSTERIP" ]
+    then
+        let CLUSTERFOUND=1
+    else
+        sleep 3
+    fi
+done
+
+
+echo '#'
+echo '# Setting up views'
+echo "# cloud-benchmark setup -c couchbase://$CLUSTERIP"
+echo '#'
+
+cloud-benchmark setup -c couchbase://$CLUSTERIP
+
+echo '#'
+echo '# Executing benchmarks...'
+echo "# cloud-benchmark run -d /data -c couchbase://$CLUSTERIP"
+echo '#'
+
+#for i in {1..100}; do
+while true; do
+    cloud-benchmark run -d /data -c couchbase://$CLUSTERIP
+    sleep 15
+done
+
